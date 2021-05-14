2cac5e4f8c52fe3dc10822230bc923afe7c8617b
oturkeybeoo <74595102+oturkeybeoo@users.noreply.github.com>
2021-05-14 22:16:50 +0700

    Update models.py
    fix Train

--- backend/train/models.py
+++ backend/train/models.py
@@ -1,15 +1,14 @@
 import django.contrib.postgres.fields
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from ..station.models import Station
 from ..utils import get_passing_station
 
 class Train(models.Model):
     name = models.CharField(max_length=120)
     number_of_seats = models.IntegerField()
-    starting_station = models.OneToOneField(Station)
-    final_station = models.OneToOneField(Station)
-    passing_stations = ArrayField(models.OneToOneField(Station))
+    starting_station = models.CharField(max_length=60)
+    final_station = models.CharField(max_length=60)
+    passing_stations = ArrayField(models.CharField(max_length=60))
 
     def __str__ (self):
         return self.name
