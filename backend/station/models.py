5c969d4e52fdff909bf92f7f2ad674e235b26fda
oturkeybeoo <74595102+oturkeybeoo@users.noreply.github.com>
2021-05-14 22:19:47 +0700

    Update models.py
    fix station

--- backend/station/models.py
+++ backend/station/models.py
@@ -4,3 +4,9 @@ from ..train import Train
 class Station(models.Model):
     name = models.CharField(max_length=60)
     available_train = models.ManyToManyField(Train)
+    
+    def __init__(self, name):
+        self.name = name
+       
+    def __str__(self):
+        return self.name
