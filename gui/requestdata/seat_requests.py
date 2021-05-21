f5a5a05c9368e249fa65300528deb2d71ababdbd
Vinh Nguyen <oturkeybeoo@gmail.com>
2021-05-21 22:37:33 +0700

    add taken seat to get_train_info

--- gui/requestdata/seat_requests.py
+++ gui/requestdata/seat_requests.py
@@ -10,9 +10,18 @@ def get_all_seat():
     all_seats = json.loads(get_all_seats.text)
     return all_seats
 
+def get_seats_of_train(train_id):
+    all_seats = requests.get(url="http://127.0.0.1:8000/seat/")
+    all_seats = json.loads(all_seats.text)
+    taken_seats = []
+    for seat in all_seats:
+        if seat.get("is_taken"):
+            taken_seats.append(seat.get('seat_number'))
+    return taken_seats
+
 def get_seat(id):
     get_seat_by_id = requests.get(url=f"http://127.0.0.1:8000/seat/{id}/")
     seat = json.loads(get_seat_by_id.text)
     return seat
 
-print(get_all_seat())
+print(get_seats_of_train(1))
--- gui/requestdata/train_requests.py
+++ gui/requestdata/train_requests.py
@@ -1,7 +1,9 @@
 import requests
 import json
+from datetime import datetime
 
 from route_requests import get_route
+from seat_requests import get_seats_of_train
 
 train_fields = ['id', 'train_name', 'train_distance']
 train_url = "http://127.0.0.1:8000/train/"
@@ -18,12 +20,13 @@ def get_train(id):
 
 def get_train_info(id):
     get_train_by_id = requests.get(url=f"http://127.0.0.1:8000/train/{id}/")
-    train = json.loads(get_train_by_id.text)
+    train = json.loads(get_train_by_id.text) 
     train_info = {
         "train_name": train.get("train_name"),
         "route_name": get_route(train.get("route_id")),
         "departing_time": train.get("departing_time"),
-        "number_of_seats": train.get("number_of_seats")
+        "number_of_seats": train.get("number_of_seats"),
+        "taken_seats": get_seats_of_train(id)
     }
     return train_info
 
@@ -38,4 +41,4 @@ def create_train(request):
     create_train = requests.post(url="http://127.0.0.1:8000/train/create/", json=request)
     return create_train.status_code
 
-print(get_train(1).get("train_name"))
+print(get_train_info(1))
